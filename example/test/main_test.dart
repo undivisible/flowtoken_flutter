898cc11dd510148cf33121680b409e874ffa7eca
Max Carter 祁明思 <undivisible@vk.com>
2026-07-26 02:03:53 -0400

    Fix FlowToken demo test import

--- example/test/main_test.dart
+++ example/test/main_test.dart
@@ -1,3 +1,4 @@
+import 'package:flowtoken_flutter/flowtoken_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flowtoken_flutter_example/main.dart';
